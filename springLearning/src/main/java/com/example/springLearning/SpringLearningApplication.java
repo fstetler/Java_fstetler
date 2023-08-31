7aef3177c616676b5e48b24f7defb6e20467cca3
Fredrik Stetler <fredrikstetler@gmail.com>
2023-08-31 11:55:55 +0200

    made everything work initial

--- springLearning/src/main/java/com/example/springLearning/SpringLearningApplication.java
+++ springLearning/src/main/java/com/example/springLearning/SpringLearningApplication.java
@@ -2,10 +2,10 @@ package com.example.springLearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringLearningApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearningApplication.class, args);
 	}
