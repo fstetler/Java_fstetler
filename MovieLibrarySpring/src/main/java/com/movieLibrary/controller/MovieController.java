0b50d7aa54b1d0348862104c6ace7832f0e8e2a0
Fredrik Stetler <fredrikstetler@gmail.com>
2023-09-04 13:53:45 +0200

    added files

--- MovieLibrarySpring/src/main/java/com/movieLibrary/controller/MovieController.java
+++ MovieLibrarySpring/src/main/java/com/movieLibrary/controller/MovieController.java
@@ -1,19 +1,44 @@
 package com.movieLibrary.controller;
 
 import com.movieLibrary.model.Movie;
+import com.movieLibrary.repository.MovieRepository;
+import com.movieLibrary.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class MovieController {
 
+    public MovieRepository movieRepository;
+
+    public MovieService movieService;
+
     @GetMapping("/getMovie")
-    public Movie getMovie() {
-        return new Movie(1, "Golden Eye", 8.5);
+    public Optional<Movie> getMovie(Movie movie) {
+        return movieService.getMovie(movie);
+    }
+
+    @GetMapping("/getAllMovies")
+    public List<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/getTest")
+    public Movie getTest() {
+        return new Movie(1, "shitmovie", 1.1);
+    }
+
+    @PostMapping("/addMovie")
+    public Movie addMovie(Movie movie) {
+        return movieRepository.save(movie);
     }
 
 }
--- MovieLibrarySpring/src/main/java/com/movieLibrary/service/MovieService.java
+++ MovieLibrarySpring/src/main/java/com/movieLibrary/service/MovieService.java
@@ -1,18 +1,35 @@
 package com.movieLibrary.service;
 
 import com.movieLibrary.model.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.movieLibrary.repository.MovieRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
 
+    @Autowired
     private MovieRepository movieRepository;
+
+    public Optional<Movie> getMovie(Movie movie) {
+        return movieRepository.findById(movie.getId());
+    }
+
+    public Movie getTest() {
+        return new Movie(1, "shitmovie", 1.1);
+    }
 
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
+    }
+
+    public Movie addMovie(Movie movie) {
+        return movieRepository.save(movie);
     }
 
 
