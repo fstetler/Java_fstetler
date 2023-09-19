6ccb101681a7d189c65c4d5219fe8a40f840281b
Fredrik Stetler <fredrikstetler@gmail.com>
2023-09-19 16:11:19 +0200

    initial RecipesRec

---
+++ recipesRecommendation/src/main/java/com/recipes/recipesRecommendation/RecipesRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.recipes.recipesRecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesRecommendationApplication.class, args);
+	}
+
+}
---
+++ recipesRecommendation/src/test/java/com/recipes/recipesRecommendation/RecipesRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipes.recipesRecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
