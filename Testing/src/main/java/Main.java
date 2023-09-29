1d187e747f1ed55e1e441fda4c1eece6363674e7
Fredrik Stetler <fredrikstetler@gmail.com>
2023-09-29 16:38:43 +0200

    Added

--- Testing/src/main/java/Main.java
+++ Testing/src/main/java/Main.java
@@ -1,12 +1,10 @@
-import anotherPackage.AnotherClass;
-import anotherPackage.AnotherInterface;
-import anotherPackage.GenericClass;
 import anotherPackage.ThreadClass;
+import justClasses.Product;
 
-
-import java.nio.file.Path;
 import java.util.ArrayList;
-import java.lang.Package;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class Main extends ThreadClass {
@@ -18,28 +16,48 @@ public class Main extends ThreadClass {
 
     public static void main(String[] args) throws Exception {
 
-        int a = 1;
-
         Main main = new Main();
-        main.start();
+
+        main.getPriority();
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Chair", 10));
+        products.add(new Product(2, "Fridge", 100));
+        products.add(new Product(3, "Chair", 10));
+        products.add(new Product(4, "TV", 15));
+        products.add(new Product(5, "Couch", 45));
+        products.add(new Product(6, "Book", 1));
 
 
+        List<Integer> listOfOnlyProductsUnder30kg = products.stream().filter(product -> product.getWeight() < 30).map(product -> product.getWeight()).toList();
 
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "first");
+        map.put(2, "second");
 
-//
-//        GenericClass<Double, Integer> genericClass = new GenericClass(1.5, "hey", 1);
-//        genericClass.print();
-//
-//        ArrayList<AnotherClass> ab = new ArrayList<>();
-//        ab.add(new AnotherClass());
-//
-//
-//        shout("coool", 1, 2.0);
-//        shout(12, "nice", 5.0);
-//
-//        Path path = Path.of("C:\\Programming\\Java_fstetler\\Testing\\src\\main\\java\\textFolder\\text.txt");
-//
-//        System.out.println(shout("nice", 0, 30.0));
+        System.out.println(map.get(1));
+
+        List<String> geyy = products.stream().map(product -> product.getProductName()).toList();
+
+        System.out.println(listOfOnlyProductsUnder30kg);
+        System.out.println(namesOfLightProductsWeightSortedStreams(products));
+
+        Integer one = 1;
+        String hey = "hey";
+        Product pro = new Product(1, "product", 50);
+        System.out.println(shout(one, hey, pro));
+
+    }
+
+    private static List<String> namesOfLightProductsWeightSortedStreams(List<Product> products) {
+
+        return products.stream()
+                .filter(product -> product.getWeight() < 30)
+                .sorted((first, second) -> first.getProductName().compareTo(second.getProductName()))
+                .map(product -> product.getProductName())
+                .distinct()
+                .toList();
+
     }
 
     public static <k, v, s> v shout (k thingToShout, v otherThingToShout, s lastThing) {
